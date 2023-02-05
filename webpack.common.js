5c9aaac06773e96e0517f6793d77f2cc08eed2db
Vili Ikäheimo <vili.vikaheim@gmail.com>
2023-02-05 18:30:14 +0200

    Add a production build

--- webpack.common.js
+++ webpack.common.js
@@ -4,21 +4,11 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   entry: './index.js',
-  mode: 'development',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'index_bundle.js',
   },
   target: 'web',
-  devServer: {
-    port: '5000',
-    static: {
-      directory: path.join(__dirname, 'public')
-    },
-    open: true,
-    hot: true,
-    liveReload: true,
-  },
   resolve: {
     extensions: ['.js', '.jsx', '.json'],
   },
@@ -29,21 +19,9 @@ module.exports = {
         exclude: /node_modules/, 
         use: 'babel-loader', 
       },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
     ],
   },
   plugins: [
-    new ESLintPlugin(),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'public', 'index.html')
     })
