ee8ffca957ede5d1013e55977364e08a773ce860
Vili Ikäheimo <vili.vikaheim@gmail.com>
2023-02-25 09:59:54 +0200

    Fix linting

--- webpack.common.js
+++ webpack.common.js
@@ -1,29 +1,29 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const ESLintPlugin = require('eslint-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path")
+const ESLintPlugin = require("eslint-webpack-plugin")
 
 module.exports = {
-  entry: './index.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'index_bundle.js',
-  },
-  target: 'web',
-  resolve: {
-    extensions: ['.js', '.jsx', '.json'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/, 
-        exclude: /node_modules/, 
-        use: 'babel-loader', 
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html')
-    })
-  ]
-};
+    entry: "./index.js",
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "index_bundle.js"
+    },
+    target: "web",
+    resolve: {
+        extensions: [".js", ".jsx", ".json"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "public", "index.html")
+        })
+    ]
+}
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,36 +1,34 @@
-const path = require('path');
-const ESLintPlugin = require('eslint-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const common = require('./webpack.common');
-const { merge } = require('webpack-merge');
+const path = require("path")
+const ESLintPlugin = require("eslint-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const common = require("./webpack.common")
+const { merge } = require("webpack-merge")
 
 module.exports = merge(common, {
-  mode: 'development',
-  devServer: {
-    port: '5000',
-    static: {
-      directory: path.join(__dirname, 'public')
+    mode: "development",
+    devServer: {
+        port: "5000",
+        static: {
+            directory: path.join(__dirname, "public")
+        },
+        open: true,
+        hot: true,
+        liveReload: true
     },
-    open: true,
-    hot: true,
-    liveReload: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
-    ]
-  },
-  plugins: [
-    new ESLintPlugin(),
-  ]
-});
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [new ESLintPlugin()]
+})
