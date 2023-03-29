e2037fd379670a627c14564233b275ed539890d1
Vili Ikäheimo <vili.vikaheim@gmail.com>
2023-03-29 22:05:50 +0300

    Add translations to 404 page

--- src/pages/Notfound/Notfound.js
+++ src/pages/Notfound/Notfound.js
@@ -1,12 +1,14 @@
 import React from "react"
+import { useTranslation } from "react-i18next"
 import "./Notfound.scss"
 const Notfound = () => {
+    const { t } = useTranslation()
     return (
         <div className="not-found-outer">
-            The page you reguested for cannot be found!
+            {t("page-not-found")}
             <img
                 src={require("../../Images/mogus.gif")}
-                alt="loading..."
+                alt={t("loading")}
                 className="not-found-image"
             />
         </div>
